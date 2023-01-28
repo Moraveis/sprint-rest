db9cb190a2612a16f6ba3e0291e103c8648c0cf9
Joao Vitor <joaovitormoraveis@gmail.com>
2023-01-27 21:10:54 -0500

    Adding Basic Exception Handler

--- src/main/java/me/joao/springrest/rest/StudentController.java
+++ src/main/java/me/joao/springrest/rest/StudentController.java
@@ -1,6 +1,11 @@
 package me.joao.springrest.rest;
 
 import me.joao.springrest.entity.Student;
+import me.joao.springrest.exceptions.StudentErrorResponse;
+import me.joao.springrest.exceptions.StudentNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,12 +28,26 @@ public class StudentController {
 
     @GetMapping("/students/{studentId}")
     public Student getStudentById(@PathVariable("studentId") Integer studentId) {
+        if (studentId < 0 || studentId >= students.size()) {
+            throw new StudentNotFoundException("Student id not found. Id = " + studentId);
+        }
+
         return this.students.get(studentId);
     }
 
     @GetMapping("/students")
     public List<Student> getStudents() {
         return this.students;
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handlingException(StudentNotFoundException studentNotFoundException) {
+        StudentErrorResponse response = new StudentErrorResponse();
+        response.setStatus(HttpStatus.NOT_FOUND.value());
+        response.setMessage(studentNotFoundException.getMessage());
+        response.setTimestamp(System.currentTimeMillis());
+
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
     }
 
     private List<Student> createStudentsStubs() {
